9d7e08fd4ddbdbfe36efeba3be08ecba79988b41
aimenAlt <ymnghost@gmail.com>
2021-12-28 14:16:32 -0800

    minor changes

--- src/main/java/com/project3/revtech/controller/CartItemProductController.java
+++ src/main/java/com/project3/revtech/controller/CartItemProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/test")
 
-public class TestController {
+public class CartItemProductController {
 
     @Autowired
     CartItemProductServiceImpl cartItemProductService;
