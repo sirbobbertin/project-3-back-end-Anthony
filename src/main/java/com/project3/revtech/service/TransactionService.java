75aaa6c943c35e2b3bed4926ef078b5465aab917
Anoel17 <57414276+Anoel17@users.noreply.github.com>
2021-12-29 12:20:58 -0500

    added Transaction

--- src/main/java/com/project3/revtech/service/TransactionService.java
+++ src/main/java/com/project3/revtech/service/TransactionService.java
@@ -1,7 +1,17 @@
 package com.project3.revtech.service;
 
+import java.util.List;
+
 import com.project3.revtech.pojo.TransactionPojo;
 
 public interface TransactionService {
-    TransactionPojo addTransaction(TransactionPojo transaction);
+
+	
+	public List<TransactionPojo> getAllTransactions();
+	public TransactionPojo getTransactionById(int transactionId);
+	public List<TransactionPojo> findAllTransactionsInCart(int cartId);
+	public TransactionPojo createTransaction(TransactionPojo tp);
+	public TransactionPojo deleteTransaction(TransactionPojo tp);
+	public TransactionPojo updateTransaction(TransactionPojo tp);
+	
 }
