ca141953b2675f6389296ca52ce9d1d035277a0b
aimenAlt <44252115+aimenAlt@users.noreply.github.com>
2021-12-29 16:15:40 -0800

    Update TransactionRepository.java

--- src/main/java/com/project3/revtech/dao/TransactionRepository.java
+++ src/main/java/com/project3/revtech/dao/TransactionRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import com.project3.revtech.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 
-
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
-	@Query("select * from transaction_details where cart_id = ?")
+// 	@Query("select * from transaction_details where cart_id = ?")
 	List<Transaction> findAllTransactionsForCart(Integer cartId);
 	
 	
