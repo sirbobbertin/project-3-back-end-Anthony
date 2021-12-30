02b3db64b169d32ce9168778c5aaf060b587cd25
aimenAlt <44252115+aimenAlt@users.noreply.github.com>
2021-12-29 16:25:19 -0800

    fix Transaction repo

--- src/main/java/com/project3/revtech/dao/TransactionRepository.java
+++ src/main/java/com/project3/revtech/dao/TransactionRepository.java
@@ -5,13 +5,10 @@ import java.util.List;
 import com.project3.revtech.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
-// 	@Query("select * from transaction_details where cart_id = ?")
-	List<Transaction> findAllTransactionsForCart(Integer cartId);
-	
-	
+    List<Transaction> findAllByCartId(int cartId);
+
 }
