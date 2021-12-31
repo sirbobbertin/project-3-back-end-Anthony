4f27c3787b35de9faed5e9682204feeecc94a2e7
Rowel Selidio <59985283+RSelidio@users.noreply.github.com>
2021-12-31 00:49:07 -0700

    Delete src/main/java/com/project3/revtech/src directory

--- src/main/java/com/project3/revtech/src/main/java/com/project3/revtech/dao/ImageControllerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project3.revtech.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project3.revtech.entity.Image;
-
-public interface ImageControllerRepository extends JpaRepository<Image, Integer> {
-
-}
--- src/main/java/com/project3/revtech/src/main/java/com/project3/revtech/entity/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.project3.revtech.entity;
-
-public enum ERole {
-  ROLE_USER,
-  ROLE_MODERATOR,
-  ROLE_ADMIN
-}
