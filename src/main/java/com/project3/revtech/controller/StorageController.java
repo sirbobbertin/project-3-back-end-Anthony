7edb0baa939b6819a3282d6ce6a4c61994d18b32
Michelle Harper <87240123+michelleyharper@users.noreply.github.com>
2022-01-02 14:53:30 -0800

    Update StorageController.java

--- src/main/java/com/project3/revtech/controller/StorageController.java
+++ src/main/java/com/project3/revtech/controller/StorageController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.project3.revtech.service.StorageService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/file")
 public class StorageController {
