d2c146572e7f66218e5c86d20d9ef38c81e4a4c6
Michelle Harper <michelle.ym.harper@gmail.com>
2022-01-10 14:46:11 -0800

    Revert "Merge pull request #77 from revature-rev-tech/teamR"
    This reverts commit 3065fca29e8a14f8921498c2bbccfcb4284a92f1, reversing
    changes made to 27366f3089c3d5d7952b89d33c57c49bbe88c7d7.

--- src/main/java/com/project3/revtech/service/UserService.java
+++ src/main/java/com/project3/revtech/service/UserService.java
@@ -1,11 +1,5 @@
 package com.project3.revtech.service;
 
-import com.project3.revtech.exception.ApplicationException;
-import com.project3.revtech.pojo.UserPojo;
-
 public interface UserService {
-	
-	UserPojo updateUserService(UserPojo userInfo) throws ApplicationException;
-	UserPojo getAUserService(int userId) throws ApplicationException;
-}
 
+}
--- src/main/java/com/project3/revtech/service/UserServiceImpl.java
+++ src/main/java/com/project3/revtech/service/UserServiceImpl.java
@@ -7,37 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-  
-  	@Autowired
-	UserRepository userRepository;
-	
-	@Autowired
-	  PasswordEncoder encoder;
-	
-	@Override
-	public UserPojo updateUserService(UserPojo userInfo) throws ApplicationException {
-		
-		 User updateUser = new User(userInfo.getUsername(), encoder.encode(userInfo.getPassword()), userInfo.getEmail(), userInfo.getFirstName(), userInfo.getLastName(),
-				                    userInfo.getAddress(), userInfo.getContact());
-		User  returnUser = userRepository.save(updateUser);
-	
-				
-		return userInfo;
-
-}
-	@Override
-	public UserPojo getAUserService(int user_id) throws ApplicationException{
-		 UserPojo userPojo = null;
-	        Optional<User> optional = userRepository.findById(user_id);
-	        if (optional.isPresent()) {
-	            User user = optional.get();
-	            userPojo = new  UserPojo(userPojo.getUser_id(), userPojo.getUsername(), 
-	            		encoder.encode(userPojo.getPassword()), userPojo.getEmail(), 
-	            		userPojo.getFirstName(), userPojo.getLastName(), userPojo.getAddress(), 
-	            		userPojo.getContact());
-	        }
-	        return  userPojo;
-		}
-
 
 }
