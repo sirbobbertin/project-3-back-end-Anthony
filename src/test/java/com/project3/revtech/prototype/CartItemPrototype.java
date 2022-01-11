5b965f4c3ab0de62dde8de5dd5030770c6b25275
Michelle YM Ng Harper <87240123+michelleyharper@users.noreply.github.com>
2022-01-10 19:08:16 -0800

    Delete src/test/java/com/project3/revtech/prototype directory

--- src/test/java/com/project3/revtech/prototype/CartItemPrototype.java
+++
@@ -1,4 +0,0 @@
-package com.project3.revtech.prototype;
-
-public class CartItemPrototype {
-}
--- src/test/java/com/project3/revtech/prototype/CartPrototype.java
+++
@@ -1,7 +0,0 @@
-package com.project3.revtech.prototype;
-
-import com.project3.revtech.entity.Cart;
-
-public class CartPrototype {
-//    public static Cart cart
-}
